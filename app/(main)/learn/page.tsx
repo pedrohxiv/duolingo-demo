92c1cb50d4d38ec0ade7eabe5ce8edca7eabfd67
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2024-04-01 15:19:36 -0300

    Implementing seed script

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -17,13 +17,13 @@ const LearnPage = async () => {
   return (
     <div className="flex gap-[48px] px-6">
       <FeedWrapper>
-        <Header title="Spanish" />
+        <Header title={userProgress.activeCourse.title} />
       </FeedWrapper>
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ id: 1, title: "Spanish", imageSrc: "/es.svg" }}
-          hearts={5}
-          points={100}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
           hasActiveSubscription={false}
         />
       </StickyWrapper>
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from "drizzle-kit";
-
 import "dotenv/config";
+
+import type { Config } from "drizzle-kit";
 
 export default {
   schema: "./db/schema.ts",
